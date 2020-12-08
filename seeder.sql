7bb84fbf8bcccfe29988d2d95d24737b3b76dd68
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-12-07 19:47:20 -0600

    init seeder

---
+++ seeder.sql
@@ -0,0 +1,5 @@
+USE employees;
+
+INSERT INTO person(name, age, dateJoined, dateUpdated) VALUES ('John Doe', '18', '2020-05-05', '2020-06-05');
+
+INSERT INTO jobs(jobTitle, salaryYearly) VALUES('janitor', '18000');
