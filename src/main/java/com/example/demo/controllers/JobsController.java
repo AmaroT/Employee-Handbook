e4e67768533151c7e90676c37cb46060abc79027
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2021-01-11 18:44:18 -0600

    adding more of person info on back end

--- src/main/java/com/example/demo/controllers/JobsController.java
+++ src/main/java/com/example/demo/controllers/JobsController.java
@@ -5,6 +5,7 @@ import com.example.demo.repositories.JobsRepository;
 import com.example.demo.repositories.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -18,12 +19,14 @@ public class JobsController {
         this.jobsRepo = jobsRepo;
         this.personRepo = personRepo;
     }
+    @GetMapping("/jobs")
     public String showJobs(Model model){
         List<Job> jobs = jobsRepo.findAll();
         model.addAttribute("jobs", jobs);
         return "jobs/jobs";
     }
 
+    @GetMapping("/jobs/search")
     public String searchJob(@RequestParam String term, Model model){
         List<Job> jobs = jobsRepo.searchByJobTitle(term);
         model.addAttribute("jobs", jobs);
--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -6,6 +6,9 @@ import com.example.demo.repositories.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersonController {
@@ -19,10 +22,20 @@ public class PersonController {
         this.jobsRepo = jobsRepo;
     }
 
-
+    //Want the user to be able to access the sign-up page
     @GetMapping("/create")
     public String createPerson(Model model){
         model.addAttribute("person", new Person());
         return "person/create";
+    }
+    //What happens when a new user submits the register form?
+    @PostMapping("/sign-up")
+    public String savePerson(@ModelAttribute Person person, @RequestParam(name = "confirmPassword") String confirmPassword,
+                             @RequestParam(name = "password") String password) {
+        if (!person.getPassword().equals(confirmPassword)) {
+            return "redirect:/login";
+        }
+        String hash = passwordEncoder.encode(person.getPassword()); //plaintext password
+
     }
 }
